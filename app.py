8519f9fe41f7bcfc4abbe125ff0be568c3e63c15
Aditya Kurne <137620112+EMPER0R7@users.noreply.github.com>
2024-06-04 16:03:24 +0530

    debug

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def upload():
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=False,host='0.0.0.0',port=5000)
